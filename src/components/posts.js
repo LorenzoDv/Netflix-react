2c08d3da1bd0e4cc17a06c6b4947d4c6a1d774f7
LorenzoDv <lorenzo.devichi@hotmail.fr>
2022-10-24 11:03:51 +0200

    first commit

---
+++ src/components/posts.js
@@ -0,0 +1,28 @@
+import { useDispatch, useSelector } from "react-redux";
+import { loadposts } from "../store/posts";
+import { useEffect } from "react";
+
+const Posts = () => {
+    const dispatch = useDispatch();
+    const posts = useSelector((state) => state.list);
+    console.log(posts)
+
+    useEffect(() => {
+        dispatch(loadposts());
+    }, [dispatch]);
+
+    return (
+        <div>
+            <h1>Posts</h1>
+            <ul>
+                {posts.map((post) => (
+                    <li key={post.id}>{post.budget}</li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default Posts;
+
+
