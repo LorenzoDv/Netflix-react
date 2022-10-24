e1492901df27e2df1980c45253d1ebd422cb6336
LorenzoDv <lorenzo.devichi@hotmail.fr>
2022-10-24 14:31:50 +0200

    update

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import configureStore from "./store/configureStore";
 import Posts from "./components/posts";
 import Navbar from "./components/Navbar/Navbar";
 import "./App.css";
+import "../src/components/scss/post.scss"
 
 const store = configureStore();
 
--- src/components/posts.js
+++ src/components/posts.js
@@ -1,6 +1,20 @@
 import { useDispatch, useSelector } from "react-redux";
 import { loadposts } from "../store/posts";
 import { useEffect } from "react";
+import React from "react";
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+// import required modules
+import { Pagination, Navigation } from "swiper";
+
+
 
 const Posts = () => {
     const dispatch = useDispatch();
@@ -18,17 +32,36 @@ const Posts = () => {
     }, [dispatch]);
 
     return (
-        <div>
+        <div className="popu">
             <h1>Popular</h1>
+            <Swiper
 
-            {PopularMovie.map((post) => (
-                <ul>
-                    <li >{post.title}</li>
-                    <li >{post.overview}</li>
-                </ul>
-            ))}
+                pagination={{ clickable: true }}
+                navigation={true}
+                slidesPerView={6}
+                scrollbar={{ draggable: true }}
+                spaceBetween={30}
+                modules={[Pagination, Navigation]}
+                className="mySwiper"
+            >
+                <div className="BoddyPopular">
 
-        </div>
+                    {PopularMovie.map((post) => (
+                        <>
+                            <SwiperSlide>
+                                <div className="Popular">
+
+                                    <img src={`https://image.tmdb.org/t/p/w500/${post.poster_path}`}></img>
+                                </div>
+
+                            </SwiperSlide>
+                        </>
+                    ))
+                    }
+                </div>
+            </Swiper>
+
+        </div >
     );
 };
 
